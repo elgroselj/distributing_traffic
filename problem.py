1bb9a7fd59df3bbc44d5d13c67d32a1df515c443
elgroselj <ema.ljubljana@gmail.com>
2023-05-04 16:50:12 +0200

    sfriziran problem z solve classi
    urejena print in draw

--- problem.py
+++ problem.py
@@ -24,6 +24,8 @@ class Problem():
             ZK = Problem.get_random_ZK(g,num_ZK,max_a,ZK_seed)
         self.ZK = ZK
         
+        self.a = [ai for _,_,ai in ZK]
+        
         unconnected_places = Problem.unconnected_places(g,ZK)
         if unconnected_places is not None:
             raise Exception(unconnected_places)
@@ -139,60 +141,10 @@ class Problem():
             col_edges.append(ei)
             data.append(-1*k)
 
-        coo = coo_array((data, (row_nodes, col_edges)), shape=(len(nodes), len(edges)))
-        return coo
+        csr = coo_array((data, (row_nodes, col_edges)), shape=(len(nodes), len(edges))).tocsr()
+        return csr
 
-    def getB(num_edges, num_ZK, factors = None):
-        B = None
-        row_edge = []
-        col_ZK = []
-        data = []
-        q = 0
-        for i in range(num_edges):
-            for j in range(num_ZK):
-                row_edge.append(i)
-                col_ZK.append(q)
-                if factors is None:
-                    data.append(1)
-                else:
-                    data.append(factors[i])
-                q += 1
-        B = coo_array((data, (row_edge, col_ZK)), shape=(num_edges, num_ZK*num_edges))
-        return B
-
-    def MtoM2(M,num_ZK):
-        num_nodes, num_edges = M.shape
-        rows = []
-        cols = []
-        data = []
-        
-        shift = 0
-        for k in range(num_ZK):
-            
-            for n in range(num_nodes):
-                for e in range(num_edges):
-                    rows.append(k*num_nodes+n)
-                    cols.append(e*num_ZK + shift)
-                    data.append(M[n,e])
-            shift += 1
-        coo = coo_array((data, (rows, cols)), shape=(num_ZK*num_nodes, num_ZK*num_edges))
-        return coo
-    
-    def nastavi_fbcmm(g,ZK,c,t):
-        f = np.repeat(t,len(ZK)) #min f*x
-        
-        B = Problem.getB(g.number_of_edges(),len(ZK)) # B*x <= c
-        
-        M = Problem.sparse_incidence_matrix(g.nodes(),g.edges()) # (M*X=M_ZK)
-        
-        a = [a for _,_,a in ZK]
-        M_ZK = Problem.sparse_incidence_matrix(g.nodes(),[(z,k) for z, k, _ in ZK],factor=a)
-        
-        M2 = Problem.MtoM2(M.todense(), len(ZK)) # M2 * x = m_ZK
-        m_ZK = M_ZK.todense().flatten('F')
-        
-        return (f,B,c,M2,m_ZK)
-    
+      
     def nodes_to_edges_path(inp, inverse = False):
         if not inverse:
             nl = inp
@@ -226,20 +178,12 @@ class Problem():
             
         return(ev)
 
-    # def edge_to_incidence_vector(e, nodes):
-    #     nv = np.zeros(len(nodes))
-        
-    #     ni = nodes.index(e[0])
-    #     nv[ni] = 1
-    #     ni = nodes.index(e[1])
-    #     nv[ni] = -1
-            
-    #     return(nv)
 
     def binary_vector_to_edges(ev, edges):
         return list(compress(edges, ev))
         
     def columns_to_paths(g,X,edges_mode=False):
+        # TODO a splitam dva avta al ne?
         paths = []
         for i in range(X.shape[1]):
             if edges_mode:
@@ -257,39 +201,133 @@ class Problem():
         nx.draw_networkx_edges(g, pos, edge_color='k', width=0.5)
         for ci, p in enumerate(paths):
             nx.draw_networkx_edges(g, pos, edgelist=p, edge_color=COLORS[ci], width=2) # highlight elist
-    
-    def solve(self,mode,silent = True):
-        if mode == "linprog_v1":
             
-#                 c @ x
-#                    such that:
-
-            #     A_ub @ x <= b_ub
-            #     A_eq @ x == b_eq
-            #     lb <= x <= ub
-            
-            f,B,c,M2,m_ZK = Problem.nastavi_fbcmm(self.g,self.ZK,self.c,self.t)
-            B = B.todense()
-            res = linprog(f, A_ub=B, b_ub=c, A_eq=M2, b_eq=m_ZK, integrality=1)
-            
-            X = None
-            if not silent:
-                if res.x is None:
-                    print(res.message)
+    def draw(p,method=None):
+        to_draw = p.results if method is None else {method: p.results[method]}
+        for k in to_draw:
+            print(k + ":")
+            result = p.results[k]
+            if not result["success"]:
+                pass
+                print(result["message"])
+            else:
+                print(result["fun"],result["message"])
+                if p.G is not None:
+                    fig, ax = ox.plot_graph_routes(p.G,result["paths"],route_colors=list(COLORS)[:len(result["paths"])])
+                    # for ci, path in enumerate(paths):
+                    #     fig, ax = ox.plot_graph_route(graph,path,route_color=COLORS[ci])
                 else:
-                    print(res.x,res.fun,res.message)
-                    X = np.array(res.x).reshape(self.g.number_of_edges(),len(self.ZK))
-                    print(X)
-                    # path0 = hf.nodes_to_edges_path(hf.binary_vector_to_edges(X[:,0],g.edges()), inverse=True)
-                    # fig, ax = ox.plot_graph_route(graph,path0)
-                    if self.G is not None:
-                        paths = Problem.columns_to_paths(self.g,X)
-                        fig, ax = ox.plot_graph_routes(self.G,paths,route_colors=list(COLORS)[:len(paths)])
-                        # for ci, path in enumerate(paths):
-                        #     fig, ax = ox.plot_graph_route(graph,path,route_color=COLORS[ci])
-                    else:
-                        paths_edges = Problem.columns_to_paths(self.g,X,edges_mode=True)
-                        Problem.my_nx_draw(self.g,paths_edges,with_labels=True,with_nodes=False)
-            self.results[mode] = {"X": X, "fun": res.fun, "message": res.message, "success": res.x is not None}
-            
+                    paths_edges = Problem.columns_to_paths(p.g,result["X"],edges_mode=True)
+                    Problem.my_nx_draw(p.g,paths_edges,with_labels=True,with_nodes=False)
+                    plt.show()
     
+    def print(p):
+        for k in p.results:
+            print(k + ":")
+            print(p.results[k]["message"], p.results[k]["fun"])
+
+class Linprog_v1():
+    def getB(num_edges, num_ZK, factors = None):
+        B = None
+        row_edge = []
+        col_ZK = []
+        data = []
+        q = 0
+        for i in range(num_edges):
+            for j in range(num_ZK):
+                row_edge.append(i)
+                col_ZK.append(q)
+                if factors is None:
+                    data.append(1)
+                else:
+                    data.append(factors[i])
+                q += 1
+        B = coo_array((data, (row_edge, col_ZK)), shape=(num_edges, num_ZK*num_edges)).tocsr()
+        return B
+    
+    def MtoM2(M,num_ZK):
+        num_nodes, num_edges = M.shape
+        rows = []
+        cols = []
+        data = []
+        
+        shift = 0
+        for k in range(num_ZK):
+            
+            for n in range(num_nodes):
+                for e in range(num_edges):
+                    rows.append(k*num_nodes+n)
+                    cols.append(e*num_ZK + shift)
+                    data.append(M[n,e])
+            shift += 1
+        csr = coo_array((data, (rows, cols)), shape=(num_ZK*num_nodes, num_ZK*num_edges)).tocsr()
+        return csr
+    
+    def nastavi_fbcmm(g,ZK,c,t):
+        f = np.repeat(t,len(ZK)) #min f*x
+        
+        B = Linprog_v1.getB(g.number_of_edges(),len(ZK)) # B*x <= c
+        
+        M = Problem.sparse_incidence_matrix(g.nodes(),g.edges()) # (M*X=M_ZK)
+        
+        a = [a for _,_,a in ZK]
+        M_ZK = Problem.sparse_incidence_matrix(g.nodes(),[(z,k) for z, k, _ in ZK],factor=a)
+        
+        M2 = Linprog_v1.MtoM2(M.todense(), len(ZK)) # M2 * x = m_ZK
+        #m_ZK = M_ZK.flatten('F')
+        m_ZK = M_ZK.reshape((g.number_of_nodes()*len(ZK),1), order='F', copy=False)
+        
+        return (f,B,c,M2,m_ZK)
+    
+    def solve(p,integrality = 1):
+        f,B,c,M2,m_ZK = Linprog_v1.nastavi_fbcmm(p.g,p.ZK,p.c,p.t)
+        m_ZK = m_ZK.todense()
+        res = linprog(f, A_ub=B, b_ub=c, A_eq=M2, b_eq=m_ZK, integrality=integrality)
+        
+        X = None
+        if res.x is not None:
+            X = np.array(res.x).reshape(p.g.number_of_edges(),len(p.ZK))
+        paths = Problem.columns_to_paths(p.g,X)
+        p.results[str(__class__)] = {"X": X, "fun": res.fun, "message": res.message, "success": res.x is not None, "paths": paths}
+
+class Linprog_v2():
+    def sestavi_QB(ZK, G, st_alternativ = 4):
+        edges = G.edges()
+        #edges_data = G.edges(data=True)
+        Q = None
+        B = None
+        
+        for i in range(len(ZK)):
+            z,k,_ = ZK[i]
+            X = nx.shortest_simple_paths(G, z, k, weight="t")
+            try:
+                for counter, path in enumerate(X):
+                    col = Problem.edges_to_binary_vector(Problem.nodes_to_edges_path(path), list(edges))
+                    Q = col if Q is None else np.column_stack([Q,col])
+                    if counter == st_alternativ-1:
+                        break
+            except Exception as e:
+                print(e)
+            else:
+                b = np.repeat(np.eye(len(ZK),1,-i), counter+1, axis=1)
+                B = b if B is None else np.column_stack([B,b])
+        
+        # t = [d["t"] for _,_,d in edges_data]
+        # c = [d["c"] for _,_,d in edges_data]
+        
+        # a = [ai for _,_,ai in ZK]
+        # return(Q,B,t,c,a)
+        return(Q,B)
+    
+    def solve(p,st_alternativ):
+        Q,B = Linprog_v2.sestavi_QB(p.ZK, p.g, st_alternativ)
+        f = np.dot(p.t, Q)
+        
+        res = linprog(f, A_ub=Q, b_ub=p.c, A_eq=B, b_eq=p.a, integrality=1)
+
+        #print(res.fun, res.x, res.message)
+        #print(res.fun, res.x, res.message)
+        Q_used = Q[:,res.x > 0] # binarni vektorji uporabljenih poti
+        paths = Problem.columns_to_paths(p.g,Q_used,edges_mode=False)
+        #TODO naredi X iz Q_used (al pa obratno??) Q_used !== X !!!!
+        p.results[str(__class__)] = {"X": Q_used, "fun": res.fun, "message": res.message, "success": res.x is not None, "paths": paths}
